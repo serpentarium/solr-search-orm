baf519efe82618777986d2b3d000785f22fdda55
Vitalii Kozlovskyi <ubombi@gmail.com>
2017-04-14 03:09:15 +0300

    Add character escaping. Add skeleton of query builder

--- pso_solr/query.py
+++ pso_solr/query.py
@@ -1,7 +1,9 @@
 """
 Solr implementation of DML QuerySet
 """
+from collections import defaultdict
 from pso.query import BaseQuerySet
+from pso.q import Condition
 
 
 class QuerySet(BaseQuerySet):
@@ -13,16 +15,38 @@ class StandardQuerySet(QuerySet):
     Query converter for Solr Standard Query Parser
     """
 
+    ESCAPE_CHARACTERS = str.maketrans({k: '\\' + k for k in [
+        '\\', '+', '-', '!', '(',  ')',
+        '{', '}', '[', ']', '^', '~', '*', '?', ':'
+    ]})
+
+    FORMAT_OPERATIONS = defaultdict(lambda: str, {
+        Condition.EQ: '"{}"'.format,
+        # TODO: Like, start/end_with ...
+    })
+
+    @classmethod
+    def escape_value(cls, value):
+        return str(value)\
+            .translate(cls.ESCAPE_CHARACTERS)\
+            .replace('&&', '\\&&')\
+            .replace('||', '\\||')\
+            .replace('AND ', '\\AND ')\
+            .replace('OR ', '\\OR ')\
+            .replace('NOT ', '\\NOT ')
+
     @classmethod
     def format_q(cls, q):
+        not_op = 'NOT ' if q.inverted else ''
         boost = '^{}'.format(q.boost) if q.boost != 1 else ''
+        field = '{}:'.format(q.field) if q.is_field else ''
         if q.is_leaf:
-            if q.is_field:
-                return "{field}:{value}".format(field=q.field, value=str(q.value))
+            value = cls.FORMAT_OPERATIONS[q.operation](cls.escape_value(q.value))
 
         else:
             childs = (cls.format_q(i) for i in q.childs)
-            return "(" + " {!s} ".format(q.operator).join(childs) + ")" + boost
+            value = "(" + " {!s} ".format(q.operator).join(childs) + ")"
+        return "".join([not_op, field, value, boost])
 
     def __str__(self):
         return self.format_q(self._filter)
